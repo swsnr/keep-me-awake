82658abb943c2f3f82cfcf8f633daa38b81a169f
Sebastian Wiesner <sebastian@swsnr.de>
2026-03-18 13:22:28 +0100

    Don't crash if build tools are missing
    That's nicer on CI, where we don't have gettext or blueprint out of the box.

--- hatch_build.py
+++ hatch_build.py
@@ -36,20 +36,14 @@ class CustomBuildHook(BuildHookInterface[BuilderConfig]):
         contents = source.read_text()
         _ = source.write_text(contents.replace("de.swsnr.keepmeawake", self.app_id))
 
-    @override
-    def initialize(self, version: str, build_data: dict[str, Any]) -> None:  # pyright: ignore[reportExplicitAny]
-        super().initialize(version, build_data)
-
-        root = Path(self.root)
-        shared_data = cast(dict[str, str], build_data["shared_data"])
-
-        for package in self.build_config.packages:
-            resources_directory = root / package / "resources"
-            blueprints = list(resources_directory.glob("**/*.blp"))
-            n_blueprints = len(blueprints)
-            self.app.display_info(
-                f"Building {n_blueprints} blueprints in package {package}"
-            )
+    def _build_blueprint(self, package: str) -> None:
+        resources_directory = Path(self.root) / package / "resources"
+        blueprints = list(resources_directory.glob("**/*.blp"))
+        n_blueprints = len(blueprints)
+        self.app.display_info(
+            f"Building {n_blueprints} blueprints in package {package}"
+        )
+        try:
             _ = run(
                 [
                     "blueprint-compiler",
@@ -60,9 +54,14 @@ class CustomBuildHook(BuildHookInterface[BuilderConfig]):
                 + [str(p) for p in blueprints],
                 check=True,
             )
+        except FileNotFoundError as error:
+            self.app.display_error(f"blueprint-compiler missing: {error}")
 
-            metainfo_file = resources_directory / "metainfo.xml"
-            self.app.display_info("Translating metainfo file")
+    def _translate_metainfo(self, package: str) -> None:
+        root = Path(self.root)
+        metainfo_file = root / package / "resources" / "metainfo.xml"
+        self.app.display_info("Translating metainfo file")
+        try:
             _ = run(
                 [
                     "msgfmt",
@@ -77,23 +76,88 @@ class CustomBuildHook(BuildHookInterface[BuilderConfig]):
                 check=True,
             )
             self._patch_app_id(metainfo_file)
+        except FileNotFoundError as error:
+            self.app.display_error(f"msgfmt missing: {error}")
+
+    def _compile_resources(self, package: str) -> None:
+        root = Path(self.root)
+        resources_directory = root / package / "resources"
+        compiled_resources = root / package / "resources.gresource"
+        self.app.display_info("Compiling GLib resources")
+        try:
+            _ = run(
+                [
+                    "glib-compile-resources",
+                    f"--sourcedir={resources_directory}",
+                    f"--target={compiled_resources}",
+                    resources_directory / "resources.gresource.xml",
+                ],
+                check=True,
+            )
+        except FileNotFoundError as error:
+            self.app.display_error(f"glib-compile-resources missing: {error}")
+
+    def _compile_message_catalogs(self, shared_data: dict[str, str]) -> None:
+        self.app.display_info("Compiling message catalogs")
+        mo_dir = Path(self.build_config.directory) / "mo"
+        mo_dir.mkdir(parents=True, exist_ok=True)
+        for po_file in (Path(self.root) / "po").glob("*.po"):
+            lang = po_file.stem
+            mo_file = (mo_dir / lang).with_suffix(".mo")
+            try:
+                _ = run(["msgfmt", "-o", str(mo_file), str(po_file)], check=True)
+            except FileNotFoundError as error:
+                self.app.display_error(f"msgfmt missing: {error}")
+                return
+            else:
+                shared_data[str(mo_file)] = (
+                    f"share/locale/{lang}/LC_MESSAGES/{self.app_id}.mo"
+                )
+
+    def _translate_desktop_file(self, shared_data: dict[str, str]) -> None:
+        self.app.display_info("Translating desktop file")
+        root = Path(self.root)
+        desktop_file = (
+            Path(self.build_config.directory) / "de.swsnr.keepmeawake.desktop"
+        )
+        try:
+            _ = run(
+                [
+                    "msgfmt",
+                    "--desktop",
+                    "--template",
+                    str(root / "de.swsnr.keepmeawake.desktop"),
+                    "-d",
+                    str(root / "po"),
+                    "--output",
+                    str(desktop_file),
+                ],
+                check=True,
+            )
+        except FileNotFoundError as error:
+            self.app.display_error(f"msgfmt missing: {error}")
+            return
+        else:
+            self._patch_app_id(desktop_file)
+            shared_data[str(desktop_file)] = f"share/applications/{self.app_id}.desktop"
+
+    @override
+    def initialize(self, version: str, build_data: dict[str, Any]) -> None:  # pyright: ignore[reportExplicitAny]
+        super().initialize(version, build_data)
+
+        root = Path(self.root)
+        shared_data = cast(dict[str, str], build_data["shared_data"])
+
+        for package in self.build_config.packages:
+            self._build_blueprint(package)
+            self._translate_metainfo(package)
 
         if self.target_name == "wheel":
             # When building a wheel build a binary resource file for Gio
             for package in self.build_config.packages:
-                resources_directory = root / package / "resources"
-                compiled_resources = root / package / "resources.gresource"
+                self._compile_resources(package)
 
-                self.app.display_info("Compiling GLib resources")
-                _ = run(
-                    [
-                        "glib-compile-resources",
-                        f"--sourcedir={resources_directory}",
-                        f"--target={compiled_resources}",
-                        resources_directory / "resources.gresource.xml",
-                    ],
-                    check=True,
-                )
+                resources_directory = root / package / "resources"
 
                 self.app.display_info("Copying translated metadata file")
                 shared_data[str(resources_directory / "metainfo.xml")] = (
@@ -122,36 +186,8 @@ class CustomBuildHook(BuildHookInterface[BuilderConfig]):
                     f"share/icons/hicolor/symbolic/apps/{self.app_id}-symbolic.svg"
                 )
 
-            self.app.display_info("Compiling message catalogs")
-            mo_dir = Path(self.build_config.directory) / "mo"
-            mo_dir.mkdir(parents=True, exist_ok=True)
-            for po_file in (root / "po").glob("*.po"):
-                lang = po_file.stem
-                mo_file = (mo_dir / lang).with_suffix(".mo")
-                _ = run(["msgfmt", "-o", str(mo_file), str(po_file)], check=True)
-                shared_data[str(mo_file)] = (
-                    f"share/locale/{lang}/LC_MESSAGES/{self.app_id}.mo"
-                )
-
-            self.app.display_info("Translating desktop file")
-            desktop_file = (
-                Path(self.build_config.directory) / "de.swsnr.keepmeawake.desktop"
-            )
-            _ = run(
-                [
-                    "msgfmt",
-                    "--desktop",
-                    "--template",
-                    str(root / "de.swsnr.keepmeawake.desktop"),
-                    "-d",
-                    str(root / "po"),
-                    "--output",
-                    str(desktop_file),
-                ],
-                check=True,
-            )
-            self._patch_app_id(desktop_file)
-            shared_data[str(desktop_file)] = f"share/applications/{self.app_id}.desktop"
+            self._compile_message_catalogs(shared_data)
+            self._translate_desktop_file(shared_data)
 
             self.app.display_info("Copying D-Bus service")
             service = root / "dbus-1" / "de.swsnr.keepmeawake.service"
