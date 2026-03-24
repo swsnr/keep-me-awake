dd182d9375ed00c540347301f25a1cb7ffa81306
Sebastian Wiesner <sebastian@swsnr.de>
2026-03-24 09:25:50 +0100

    Create resources output directory

--- hatch_build.py
+++ hatch_build.py
@@ -77,6 +77,7 @@ class CustomBuildHook(BuildHookInterface[BuilderConfig]):
 
         output_directory = Path(self.build_config.directory)
         resources_out_directory = output_directory / "resources"
+        resources_out_directory.mkdir(parents=True)
 
         if os.environ.get("SKIP_BLUEPRINT") != "1":
             blueprints = list(resources_directory.glob("**/*.blp"))
